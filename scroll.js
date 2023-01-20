b60f6ce6d375d7174caae88eba3527695365b9fd
krzyvy <118526617+krzyvy@users.noreply.github.com>
2023-01-20 09:57:48 +0000

    new file:   img/up.png 	modified:   index.html 	new file:   jquery.scrollTo.min.js 	modified:   regulamin.html 	new file:   scroll.js 	modified:   style.css

---
+++ scroll.js
@@ -0,0 +1,18 @@
+jQuery(function($)
+		{
+            $.scrollTo(0);
+			
+			$('#link1').click(function() { $.scrollTo($('#admini'), 500); });
+			$('#link2').click(function() { $.scrollTo($('#shop'), 500); });
+	
+			$('.scrollup').click(function() { $.scrollTo($('body'), 500); });
+		}
+		);
+		
+		$(window).scroll(function()
+		{
+			if($(this).scrollTop()>300) $('.scrollup').fadeIn();
+			else $('.scrollup').fadeOut();		
+		}
+		);
+	
